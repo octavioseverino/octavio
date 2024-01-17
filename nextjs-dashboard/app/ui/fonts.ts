f310933eb53e5f91eb09213558962b6cfd57d77e
octavio-severino <151866762+octavio-severino@users.noreply.github.com>
2024-01-17 18:41:13 -0400

    first commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,3 @@
+import {Lusitana, Montserrat} from 'next/font/google'
+export const montserrat = Montserrat ({ subsets: ['latin'], weight: '400'})
+export const luisitana = Lusitana ({ subsets: ['latin'], weight: '400'})
