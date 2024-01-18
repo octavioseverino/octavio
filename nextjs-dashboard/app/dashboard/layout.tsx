37ad15ece4fbb8dbca9bfe1df3f3b422b2962735
octavio-severino <151866762+octavio-severino@users.noreply.github.com>
2024-01-18 19:21:14 -0400

    Cambios
    Cambios importantes

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout ({ children } : {children: React.ReactNode}) {
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
-            { children }
+            
         </section>
     )
 }
